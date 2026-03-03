023962a989e27cdd76118ac287ab60a95e946985
Mohamed Ashraf <mohamedashraf1829982@gmail.com>
2026-03-03 13:37:21 +0400

    forgot

--- Minecraft.Client/stdafx.h
+++ Minecraft.Client/stdafx.h
@@ -308,7 +308,7 @@ typedef XUID GameSessionUID;
 	// DecalOverdose: DONT (according to localcc)
 	//#include "Windows64/Iggy/include/iggy.h"
 	//#include "Windows64/Iggy/gdraw/gdraw_d3d11.h"
-	#include "Windows64/Windows64_UIController.h"
+	//#include "Windows64/Windows64_UIController.h"
 #elif defined __PSVITA__
 	#include "PSVita/PSVita_App.h"
 	#include "PSVitaMedia/strings.h"		// TODO - create PSVita-specific version of this
