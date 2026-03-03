64437b5e4e368af17518dc81f522be200923af76
Tropical <42101043+tropicaaal@users.noreply.github.com>
2026-03-03 16:08:17 -0600

    fix: UB in `DemoLevel`

--- Minecraft.Client/DemoLevel.h
+++ Minecraft.Client/DemoLevel.h
@@ -4,7 +4,8 @@
 class DemoLevel : public Level
 {
 private:
-	static const __int64 DEMO_LEVEL_SEED = 0;	// 4J - TODO - was "Don't Look Back".hashCode();
+    static const __int64 DEMO_LEVEL_SEED = 0;	// 4J - TODO - was "Don't Look Back".hashCode();
+    static const LevelSettings DEMO_LEVEL_SETTINGS;
     static const int DEMO_SPAWN_X = 796;
     static const int DEMO_SPAWN_Y = 72;
     static const int DEMO_SPAWN_Z = -731;
