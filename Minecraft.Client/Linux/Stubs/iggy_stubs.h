693512429485f3ad8ada45c6a9d4ab566b1d46cf
Tropical <42101043+tropicaaal@users.noreply.github.com>
2026-03-03 18:16:52 -0600

    fix: inline iggy stubs

--- Minecraft.Client/Linux/Stubs/iggy_stubs.h
+++ Minecraft.Client/Linux/Stubs/iggy_stubs.h
@@ -8,37 +8,37 @@
 
 #define STUBBED {}
 
-RADEXPFUNC IggyValuePath * RADEXPLINK IggyPlayerRootPath(Iggy *f) {
+RADEXPFUNC inline IggyValuePath * RADEXPLINK IggyPlayerRootPath(Iggy *f) {
   STUBBED;
   return nullptr;
 }
 
-RADEXPFUNC IggyResult RADEXPLINK IggyPlayerCallMethodRS(Iggy *f, IggyDataValue *result, IggyValuePath *target, IggyName methodname, S32 numargs, IggyDataValue *args) {
+RADEXPFUNC inline IggyResult RADEXPLINK IggyPlayerCallMethodRS(Iggy *f, IggyDataValue *result, IggyValuePath *target, IggyName methodname, S32 numargs, IggyDataValue *args) {
   STUBBED;
   return IGGY_RESULT_SUCCESS;
 }
 
-RADEXPFUNC void RADEXPLINK IggyPlayerDestroy(Iggy *player) {
+RADEXPFUNC inline void RADEXPLINK IggyPlayerDestroy(Iggy *player) {
   STUBBED;
 }
-RADEXPFUNC void RADEXPLINK IggyPlayerSetDisplaySize(Iggy *f, S32 w, S32 h) {
-  STUBBED;
-}
-
-RADEXPFUNC void RADEXPLINK IggyPlayerDrawTilesStart(Iggy *f) {
+RADEXPFUNC inline void RADEXPLINK IggyPlayerSetDisplaySize(Iggy *f, S32 w, S32 h) {
   STUBBED;
 }
 
-RADEXPFUNC void RADEXPLINK IggyPlayerDrawTile(Iggy *f, S32 x0, S32 y0, S32 x1, S32 y1, S32 padding) {
+RADEXPFUNC inline void RADEXPLINK IggyPlayerDrawTilesStart(Iggy *f) {
   STUBBED;
 }
-RADEXPFUNC void RADEXPLINK IggyPlayerDrawTilesEnd(Iggy *f) {
+
+RADEXPFUNC inline void RADEXPLINK IggyPlayerDrawTile(Iggy *f, S32 x0, S32 y0, S32 x1, S32 y1, S32 padding) {
+  STUBBED;
+}
+RADEXPFUNC inline void RADEXPLINK IggyPlayerDrawTilesEnd(Iggy *f) {
   STUBBED;
 }
 
 int thing = 0;
 
-RADEXPFUNC Iggy * RADEXPLINK IggyPlayerCreateFromMemory(
+RADEXPFUNC inline Iggy * RADEXPLINK IggyPlayerCreateFromMemory(
                                 void const *           data,
                                 U32                    data_size_in_bytes,
                                 IggyPlayerConfig      *config) {
@@ -47,107 +47,107 @@ RADEXPFUNC Iggy * RADEXPLINK IggyPlayerCreateFromMemory(
 }
 
 
-RADEXPFUNC void RADEXPLINK IggyPlayerInitializeAndTickRS(Iggy *player) {
+RADEXPFUNC inline void RADEXPLINK IggyPlayerInitializeAndTickRS(Iggy *player) {
   STUBBED;
 }
 
 IggyProperties properties;
 
-RADEXPFUNC IggyProperties * RADEXPLINK IggyPlayerProperties(Iggy *player) {
+RADEXPFUNC inline IggyProperties * RADEXPLINK IggyPlayerProperties(Iggy *player) {
   STUBBED;
   return &properties;
 }
-RADEXPFUNC void RADEXPLINK IggyPlayerSetUserdata(Iggy *player, void *userdata) {
+RADEXPFUNC inline void RADEXPLINK IggyPlayerSetUserdata(Iggy *player, void *userdata) {
   STUBBED;
 }
-RADEXPFUNC IggyName RADEXPLINK IggyPlayerCreateFastName(Iggy *f, IggyUTF16 const *name, S32 len) {
+RADEXPFUNC inline IggyName RADEXPLINK IggyPlayerCreateFastName(Iggy *f, IggyUTF16 const *name, S32 len) {
   STUBBED;
   return 0;
 }
-RADEXPFUNC rrbool RADEXPLINK IggyDebugGetMemoryUseInfo(Iggy *player, IggyLibrary lib, char const *category_string, S32 category_stringlen, S32 iteration, IggyMemoryUseInfo *data) {
+RADEXPFUNC inline rrbool RADEXPLINK IggyDebugGetMemoryUseInfo(Iggy *player, IggyLibrary lib, char const *category_string, S32 category_stringlen, S32 iteration, IggyMemoryUseInfo *data) {
   STUBBED;
   return false;
 }
-RADEXPFUNC rrbool RADEXPLINK IggyPlayerReadyToTick(Iggy *player) {
+RADEXPFUNC inline rrbool RADEXPLINK IggyPlayerReadyToTick(Iggy *player) {
   STUBBED;
   return false;
 }
-RADEXPFUNC void RADEXPLINK IggyPlayerTickRS(Iggy *player) {
+RADEXPFUNC inline void RADEXPLINK IggyPlayerTickRS(Iggy *player) {
   STUBBED;
 }
-RADEXPFUNC void RADEXPLINK IggyPlayerDraw(Iggy *f) {
+RADEXPFUNC inline void RADEXPLINK IggyPlayerDraw(Iggy *f) {
   STUBBED;
 }
-RADEXPFUNC void RADEXPLINK IggyMakeEventKey(IggyEvent *event, IggyKeyevent event_type, IggyKeycode keycode, IggyKeyloc keyloc) {
+RADEXPFUNC inline void RADEXPLINK IggyMakeEventKey(IggyEvent *event, IggyKeyevent event_type, IggyKeycode keycode, IggyKeyloc keyloc) {
   STUBBED;
 }
-RADEXPFUNC rrbool RADEXPLINK IggyPlayerDispatchEventRS(Iggy *player, IggyEvent *event, IggyEventResult *result) {
-  STUBBED;
-  return false;
-}
-RADEXPFUNC void RADEXPLINK IggyFontRemoveUTF8(const char *fontname, S32 namelen_in_bytes, U32 fontflags) {
-  STUBBED;
-}
-RADEXPFUNC void RADEXPLINK IggyFontInstallBitmapUTF8(const IggyBitmapFontProvider *bmf, const char *fontname, S32 namelen_in_bytes, U32 fontflags) {
-  STUBBED;
-}
-RADEXPFUNC void RADEXPLINK IggyFontSetIndirectUTF8(const char *request_name, S32 request_namelen, U32 request_flags, const char *result_name, S32 result_namelen, U32 result_flags) {
-  STUBBED;
-}
-RADEXPFUNC void RADEXPLINK IggyFontInstallTruetypeUTF8(const void *truetype_storage, S32 ttc_index, const char *fontname, S32 namelen_in_bytes, U32 fontflags) {
-  STUBBED;
-}
-RADEXPFUNC rrbool RADEXPLINK IggyValuePathMakeNameRef(IggyValuePath *result, IggyValuePath *parent, char const *text_utf8) {
+RADEXPFUNC inline rrbool RADEXPLINK IggyPlayerDispatchEventRS(Iggy *player, IggyEvent *event, IggyEventResult *result) {
   STUBBED;
   return false;
 }
-RADEXPFUNC IggyResult RADEXPLINK IggyValueGetBooleanRS(IggyValuePath *var, IggyName sub_name, char const *sub_name_utf8, rrbool *result) {
+RADEXPFUNC inline void RADEXPLINK IggyFontRemoveUTF8(const char *fontname, S32 namelen_in_bytes, U32 fontflags) {
+  STUBBED;
+}
+RADEXPFUNC inline void RADEXPLINK IggyFontInstallBitmapUTF8(const IggyBitmapFontProvider *bmf, const char *fontname, S32 namelen_in_bytes, U32 fontflags) {
+  STUBBED;
+}
+RADEXPFUNC inline void RADEXPLINK IggyFontSetIndirectUTF8(const char *request_name, S32 request_namelen, U32 request_flags, const char *result_name, S32 result_namelen, U32 result_flags) {
+  STUBBED;
+}
+RADEXPFUNC inline void RADEXPLINK IggyFontInstallTruetypeUTF8(const void *truetype_storage, S32 ttc_index, const char *fontname, S32 namelen_in_bytes, U32 fontflags) {
+  STUBBED;
+}
+RADEXPFUNC inline rrbool RADEXPLINK IggyValuePathMakeNameRef(IggyValuePath *result, IggyValuePath *parent, char const *text_utf8) {
+  STUBBED;
+  return false;
+}
+RADEXPFUNC inline IggyResult RADEXPLINK IggyValueGetBooleanRS(IggyValuePath *var, IggyName sub_name, char const *sub_name_utf8, rrbool *result) {
   STUBBED;
   return IGGY_RESULT_SUCCESS;
 }
-RADEXPFUNC void RADEXPLINK IggyFontInstallTruetypeFallbackCodepointUTF8(const char *fontname, S32 len, U32 fontflags, S32 fallback_codepoint) {
+RADEXPFUNC inline void RADEXPLINK IggyFontInstallTruetypeFallbackCodepointUTF8(const char *fontname, S32 len, U32 fontflags, S32 fallback_codepoint) {
   STUBBED;
 }
-RADEXPFUNC IggyResult RADEXPLINK IggyValueGetF64RS(IggyValuePath *var, IggyName sub_name, char const *sub_name_utf8, F64 *result) {
+RADEXPFUNC inline IggyResult RADEXPLINK IggyValueGetF64RS(IggyValuePath *var, IggyName sub_name, char const *sub_name_utf8, F64 *result) {
   STUBBED;
   return IGGY_RESULT_SUCCESS;
 }
-RADEXPFUNC rrbool RADEXPLINK IggyValueSetBooleanRS(IggyValuePath *var, IggyName sub_name, char const *sub_name_utf8, rrbool value) {
+RADEXPFUNC inline rrbool RADEXPLINK IggyValueSetBooleanRS(IggyValuePath *var, IggyName sub_name, char const *sub_name_utf8, rrbool value) {
   STUBBED;
   return false;
 }
-RADEXPFUNC void RADEXPLINK IggyInit(IggyAllocator *allocator) {
+RADEXPFUNC inline void RADEXPLINK IggyInit(IggyAllocator *allocator) {
   STUBBED;
 }
-RADEXPFUNC void RADEXPLINK IggySetWarningCallback(Iggy_WarningFunction *error, void *user_callback_data) {
+RADEXPFUNC inline void RADEXPLINK IggySetWarningCallback(Iggy_WarningFunction *error, void *user_callback_data) {
   STUBBED;
 }
-RADEXPFUNC void RADEXPLINK IggySetTraceCallbackUTF8(Iggy_TraceFunctionUTF8 *trace_utf8, void *user_callback_data) {
+RADEXPFUNC inline void RADEXPLINK IggySetTraceCallbackUTF8(Iggy_TraceFunctionUTF8 *trace_utf8, void *user_callback_data) {
   STUBBED;
 }
-RADEXPFUNC void RADEXPLINK IggySetFontCachingCalculationBuffer(
+RADEXPFUNC inline void RADEXPLINK IggySetFontCachingCalculationBuffer(
     S32 max_chars,
     void *optional_temp_buffer,
     S32 optional_temp_buffer_size_in_bytes) {
   STUBBED;
 }
-RADEXPFUNC void RADEXPLINK IggySetCustomDrawCallback(Iggy_CustomDrawCallback *custom_draw, void *user_callback_data) {
+RADEXPFUNC inline void RADEXPLINK IggySetCustomDrawCallback(Iggy_CustomDrawCallback *custom_draw, void *user_callback_data) {
   STUBBED;
 }
-RADEXPFUNC void RADEXPLINK IggySetAS3ExternalFunctionCallbackUTF16(Iggy_AS3ExternalFunctionUTF16 *as3_external_function_utf16, void *user_callback_data) {
+RADEXPFUNC inline void RADEXPLINK IggySetAS3ExternalFunctionCallbackUTF16(Iggy_AS3ExternalFunctionUTF16 *as3_external_function_utf16, void *user_callback_data) {
   STUBBED;
 }
-RADEXPFUNC void RADEXPLINK IggyMakeEventMouseMove(IggyEvent *event, S32 x, S32 y) {
+RADEXPFUNC inline void RADEXPLINK IggyMakeEventMouseMove(IggyEvent *event, S32 x, S32 y) {
   STUBBED;
 }
-RADEXPFUNC void RADEXPLINK IggySetTextureSubstitutionCallbacks(Iggy_TextureSubstitutionCreateCallback *texture_create, Iggy_TextureSubstitutionDestroyCallback *texture_destroy, void *user_callback_data) {
+RADEXPFUNC inline void RADEXPLINK IggySetTextureSubstitutionCallbacks(Iggy_TextureSubstitutionCreateCallback *texture_create, Iggy_TextureSubstitutionDestroyCallback *texture_destroy, void *user_callback_data) {
   STUBBED;
 }
-RADEXPFUNC void * RADEXPLINK IggyPlayerGetUserdata(Iggy *player) {
+RADEXPFUNC inline void * RADEXPLINK IggyPlayerGetUserdata(Iggy *player) {
   STUBBED;
   return 0;
 }
-RADEXPFUNC  IggyLibrary  RADEXPLINK IggyLibraryCreateFromMemoryUTF16(
+RADEXPFUNC inline  IggyLibrary  RADEXPLINK IggyLibraryCreateFromMemoryUTF16(
                                 IggyUTF16 const *      url_utf16_null_terminated,
                                 void const *           data,
                                 U32                    data_size_in_bytes,
@@ -155,7 +155,7 @@ RADEXPFUNC  IggyLibrary  RADEXPLINK IggyLibraryCreateFromMemoryUTF16(
   STUBBED;
   return 0;
 }
-RADEXPFUNC void RADEXPLINK IggyLibraryDestroy(IggyLibrary lib) {
+RADEXPFUNC inline void RADEXPLINK IggyLibraryDestroy(IggyLibrary lib) {
   STUBBED;
 }
 
