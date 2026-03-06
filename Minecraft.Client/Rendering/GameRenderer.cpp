bf022d12f230354cfd90c9e08888be51103837a4
JuiceyDev <me@juicey.dev>
2026-03-07 00:11:28 +0100

    wawa

--- Minecraft.Client/Rendering/GameRenderer.cpp
+++ Minecraft.Client/Rendering/GameRenderer.cpp
@@ -1002,8 +1002,9 @@ int GameRenderer::getLightTexture(int iPad, Level *level)
 	return lightTexture[iPad]; // 4J-JEV: Changing to Per Player lighting textures.
 }
 
-void GameRenderer::render(float a = 1, bool bFirst)
+void GameRenderer::render(float a, bool bFirst)
 {
+	a = 1;
 	if( _updateLightTexture && bFirst) updateLightTexture(a);
 	if (Display::isActive())
 	{
