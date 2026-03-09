5f1cdc1a3c5d49ce056be8e46ad41e8e0e93bcf3
notmatthewbeshay <92357869+NotMachow@users.noreply.github.com>
2026-03-10 01:15:08 +1100

    Remove Win32 byte types from memory texture loaders

--- Minecraft.Client/Textures/BufferedImage.h
+++ Minecraft.Client/Textures/BufferedImage.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstdint>
 
 
 class Graphics;
@@ -17,7 +18,7 @@ public:
 	BufferedImage(int width,int height,int type);
 	BufferedImage(const std::wstring& File, bool filenameHasExtension = false, bool bTitleUpdateTexture=false, const std::wstring &drive =L"");	// 4J added
 	BufferedImage(DLCPack *dlcPack, const std::wstring& File, bool filenameHasExtension = false ); // 4J Added
-	BufferedImage(BYTE *pbData, DWORD dwBytes);	// 4J added
+	BufferedImage(std::uint8_t *pbData, std::uint32_t dataBytes);	// 4J added
 	~BufferedImage();
 
 	int getWidth();	
