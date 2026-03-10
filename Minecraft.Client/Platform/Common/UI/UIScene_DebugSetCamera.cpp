d94f396fcfce355e48187613124b6cd52ad4e862
notmatthewbeshay <92357869+NotMachow@users.noreply.github.com>
2026-03-11 00:09:09 +1100

    Remove Win32 wide char aliases from debug UI scenes

--- Minecraft.Client/Platform/Common/UI/UIScene_DebugSetCamera.cpp
+++ Minecraft.Client/Platform/Common/UI/UIScene_DebugSetCamera.cpp
@@ -29,21 +29,21 @@ UIScene_DebugSetCamera::UIScene_DebugSetCamera(int iPad, void *initData, UILayer
 		currentPosition->m_elev = pMinecraft->localplayers[playerNo]->xRot;
 	}
 
-	WCHAR TempString[256];
+	wchar_t TempString[256];
 
-	swprintf( (WCHAR *)TempString, 256, L"%f", currentPosition->m_camX);
+	swprintf(TempString, 256, L"%f", currentPosition->m_camX);
 	m_textInputX.init(TempString, eControl_CamX);
 
-	swprintf( (WCHAR *)TempString, 256, L"%f", currentPosition->m_camY);
+	swprintf(TempString, 256, L"%f", currentPosition->m_camY);
 	m_textInputY.init(TempString, eControl_CamY);
 
-	swprintf( (WCHAR *)TempString, 256, L"%f", currentPosition->m_camZ);
+	swprintf(TempString, 256, L"%f", currentPosition->m_camZ);
 	m_textInputZ.init(TempString, eControl_CamZ);
 
-	swprintf( (WCHAR *)TempString, 256, L"%f", currentPosition->m_yRot);
+	swprintf(TempString, 256, L"%f", currentPosition->m_yRot);
 	m_textInputYRot.init(TempString, eControl_YRot);
 
-	swprintf( (WCHAR *)TempString, 256, L"%f", currentPosition->m_elev);
+	swprintf(TempString, 256, L"%f", currentPosition->m_elev);
 	m_textInputElevation.init(TempString, eControl_Elevation);
 
 	m_checkboxLockPlayer.init(L"Lock Player", eControl_LockPlayer, app.GetFreezePlayers());
