cdd2f39b3360ac3ac979e6ee072dfda29c6d8c4d
Tropical <42101043+tropicaaal@users.noreply.github.com>
2026-03-22 02:29:12 -0500

    format common sound engine folder

--- Minecraft.Client/Platform/Common/Audio/Consoles_SoundEngine.cpp
+++ Minecraft.Client/Platform/Common/Audio/Consoles_SoundEngine.cpp
@@ -1,38 +1,25 @@
 #include "../../Minecraft.World/Platform/stdafx.h"
 #include "Consoles_SoundEngine.h"
 
-
-bool ConsoleSoundEngine::GetIsPlayingStreamingCDMusic()				
-{ 
-	return m_bIsPlayingStreamingCDMusic;
+bool ConsoleSoundEngine::GetIsPlayingStreamingCDMusic() {
+    return m_bIsPlayingStreamingCDMusic;
 }
-bool ConsoleSoundEngine::GetIsPlayingStreamingGameMusic()			
-{
-	return m_bIsPlayingStreamingGameMusic;
+bool ConsoleSoundEngine::GetIsPlayingStreamingGameMusic() {
+    return m_bIsPlayingStreamingGameMusic;
 }
-void ConsoleSoundEngine::SetIsPlayingStreamingCDMusic(bool bVal)	
-{
-	m_bIsPlayingStreamingCDMusic=bVal;
+void ConsoleSoundEngine::SetIsPlayingStreamingCDMusic(bool bVal) {
+    m_bIsPlayingStreamingCDMusic = bVal;
 }
-void ConsoleSoundEngine::SetIsPlayingStreamingGameMusic(bool bVal)	
-{
-	m_bIsPlayingStreamingGameMusic=bVal;
+void ConsoleSoundEngine::SetIsPlayingStreamingGameMusic(bool bVal) {
+    m_bIsPlayingStreamingGameMusic = bVal;
 }
-bool ConsoleSoundEngine::GetIsPlayingEndMusic()						
-{ 
-	return m_bIsPlayingEndMusic;
+bool ConsoleSoundEngine::GetIsPlayingEndMusic() { return m_bIsPlayingEndMusic; }
+bool ConsoleSoundEngine::GetIsPlayingNetherMusic() {
+    return m_bIsPlayingNetherMusic;
 }
-bool ConsoleSoundEngine::GetIsPlayingNetherMusic()					
-{ 
-	return m_bIsPlayingNetherMusic;
+void ConsoleSoundEngine::SetIsPlayingEndMusic(bool bVal) {
+    m_bIsPlayingEndMusic = bVal;
 }
-void ConsoleSoundEngine::SetIsPlayingEndMusic(bool bVal)			
-{
-	m_bIsPlayingEndMusic=bVal;
+void ConsoleSoundEngine::SetIsPlayingNetherMusic(bool bVal) {
+    m_bIsPlayingNetherMusic = bVal;
 }
-void ConsoleSoundEngine::SetIsPlayingNetherMusic(bool bVal)			
-{
-	m_bIsPlayingNetherMusic=bVal;
-}
-
-
