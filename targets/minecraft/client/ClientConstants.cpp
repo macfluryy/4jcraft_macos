3999781e86f6ab594ce622c993fc598063fb494f
macfluryy <nkolotovkinrus@gmail.com>
2026-04-07 15:16:15 +0300

    feat: change version string to 'Minecraft Apple Silicon Port'

--- targets/minecraft/client/ClientConstants.cpp
+++ targets/minecraft/client/ClientConstants.cpp
@@ -3,5 +3,5 @@
 #include "app/common/src/BuildVer/BuildVer.h"
 
 const std::wstring ClientConstants::VERSION_STRING =
-    std::wstring(L"Minecraft Xbox ") + VER_FILEVERSION_STR_W +
+    std::wstring(L"Minecraft Apple Silicon Port ") + VER_FILEVERSION_STR_W +
     std::wstring(L" (4jcraft)");  //+ SharedConstants::VERSION_STRING;
